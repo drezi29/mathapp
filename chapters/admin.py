ba4ab04dc52a61d3bb2d09f17dec745633d0562e
drezi29 <julia.drezek98@gmail.com>
2023-04-03 19:06:00 +0200

    adjusting models and admin site

--- chapters/admin.py
+++ chapters/admin.py
@@ -1,10 +1,18 @@
+from django import forms
 from django.contrib import admin
-from .models import Chapter, Topic
+from django.core.exceptions import ValidationError
+from .models import Chapter, NoteElement, Note, Topic 
 
 class ChapterAdmin(admin.ModelAdmin):
-    list_display = ['name', 'lic_class', 'tech_class', 'is_extended', 'order']
+    list_display = ['name', 'program_class', 'is_extended', 'order']
 admin.site.register(Chapter, ChapterAdmin)
 
 class TopicAdmin(admin.ModelAdmin):
     list_display = ['name', 'chapter', 'is_extended', 'order']
 admin.site.register(Topic, TopicAdmin)
+
+admin.site.register(Note)
+
+class NoteElementAdmin(admin.ModelAdmin):
+    list_display = ['note', 'name', 'order']
+admin.site.register(NoteElement, NoteElementAdmin)
