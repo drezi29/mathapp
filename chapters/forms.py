e9c123e1235a2e77dfec8fad183064da74f3ed2a
drezi29 <julia.drezek98@gmail.com>
2023-04-18 16:20:17 +0200

    removing chapter filtering, add bootstrap

--- chapters/forms.py
+++ chapters/forms.py
@@ -1,19 +1,1 @@
 from django import forms
-from .models import Chapter
-
-CLASSES_CHOICES = (
-    (1, "Klasa 1"),
-    (2, "Klasa 2"),
-    (3, "Klasa 3"),
-    (4, "Klasa 4"),)
-
-class ChaptersFilterForm(forms.Form):
-    classes = forms.MultipleChoiceField(choices=CLASSES_CHOICES, label="Klasy")
-    # elementary_level = forms.BooleanField(required=False, label='Poziom podstawowy')
-    # extended_level = forms.BooleanField(required=False, label='Pokaż rozdziały z poziomu rozszerzonego')
-    
-    class Meta:
-        model = Chapter
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
--- chapters/views.py
+++ chapters/views.py
@@ -1,32 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
-from .forms import ChaptersFilterForm
 from .models import Chapter, Topic
 
 
 class ChapterListView(ListView):
     model = Chapter
-    # queryset = Chapter.objects.filter(pk__in=Topic.objects.all().values_list('chapter'))
 
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = object_list if object_list is not None else self.object_list
         classes_section = [1,2,3,4]
-        filter_form = ChaptersFilterForm(self.request.GET)
-
-        if filter_form.is_valid():
-            selected_classes = filter_form.cleaned_data.get('classes', '')
-            # extended_level = filter_form.cleaned_data.get('extended_level', '')
-
-            classes_section = list(map(lambda x : int(x), selected_classes))
-            if len(selected_classes)>0:
-                queryset = queryset.filter(program_class__in=classes_section)
-            # if not(extended_level):
-            #     queryset = queryset.filter(is_extended=False)
 
         return super().get_context_data(
-            filter_form=filter_form,
             chapters=queryset,
             classes_section=classes_section,
             **kwargs)
