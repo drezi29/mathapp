571ecc26ae0375d1cbd4e5d827b213356e826b3f
drezi29 <julia.drezek98@gmail.com>
2023-07-22 22:43:51 +0200

    fix(note page): improvements - tile color and buttons with link to test

--- notes/views.py
+++ notes/views.py
@@ -7,4 +7,4 @@ class NoteView(View):
     def get(self, request, pk):
         note = Note.objects.get(topic=pk)
         note_elements = note.noteelement_set.all()
-        return render(request, 'chapters/note.html', {"note_elements": note_elements, "note": note})
+        return render(request, 'chapters/note.html', {"note_elements": note_elements, "note": note, "topic_id": pk})
