98546cfadb4f1a87cdf76616045d5fc2b0f3e909
drezi29 <julia.drezek98@gmail.com>
2023-07-23 22:27:37 +0200

    feat(exercise): admin site with comfortable creating exercise and steps

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,5 +1,22 @@
+import nested_admin
 from django.contrib import admin
-from .models import Exercise
+from .models import Exercise, Step
 
 
-admin.site.register(Exercise)
+class StepAdmin(admin.ModelAdmin):
+    list_display = ['exercise', 'title', 'instruction', 'order']
+
+
+admin.site.register(Step, StepAdmin)
+
+
+class StepInline(nested_admin.NestedStackedInline):
+    model = Step
+    sortable_field_name = "title"
+
+
+class ExerciseAdmin(nested_admin.NestedModelAdmin):
+    inlines = [StepInline]
+
+
+admin.site.register(Exercise, ExerciseAdmin)
