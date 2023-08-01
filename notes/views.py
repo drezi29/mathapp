d71a559112245deadba8cc1b3f0909136a79a492
drezi29 <julia.drezek98@gmail.com>
2023-08-01 23:31:17 +0200

    feat(navigation): navigation buttons

--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Note
+from chapters.models import Topic
 
 
 class NoteView(View):
     def get(self, request, pk):
         note = Note.objects.get(topic=pk)
         note_elements = note.noteelement_set.all()
-        return render(request, 'chapters/note.html', {"note_elements": note_elements, "note": note, "topic_id": pk, "elements_amount": len(note_elements)})
+        chapter_id = Topic.objects.get(id=pk).chapter.id
+        return render(request,
+                      'chapters/note.html',
+                      {
+                          "note_elements": note_elements,
+                          "note": note, "topic_id": pk,
+                          "elements_amount": len(note_elements),
+                          "chapter_id": chapter_id
+                      }
+                      )
